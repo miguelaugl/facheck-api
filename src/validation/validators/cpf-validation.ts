326e3abd274c6497afe922c4ca6f2f45fa54e205
Miguel <miguelaugl@outlook.com>
2021-09-26 22:02:24 -0300

    feat: ensure CpfValidation returns an error if validation fails

---
+++ src/validation/validators/cpf-validation.ts
@@ -0,0 +1,13 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { CpfValidator, Validation } from '@/validation/protocols'
+
+export class CpfValidation implements Validation {
+  constructor (
+    private readonly fieldName: string,
+    private readonly cpfValidator: CpfValidator,
+  ) {}
+
+  validate (input: any): Error {
+    return new InvalidParamError(this.fieldName)
+  }
+}
