b2b71717c271ce483a4bb12703d8f19518585ce9
Miguel <miguelaugl@outlook.com>
2021-09-26 21:55:27 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/email-validation.spec.ts
+++ src/validation/validators/email-validation.spec.ts
@@ -3,15 +3,31 @@ import { EmailValidator } from '@/validation/protocols'
 
 import { EmailValidation } from './email-validation'
 
+type SutTypes = {
+  sut: EmailValidation
+  emailValidatorStub: EmailValidator
+}
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator()
+  const sut = new EmailValidation('email', emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub,
+  }
+}
 describe('Email Validation', () => {
   it('should return an InvalidParamError if validation fails', () => {
-    class EmailValidatorStub implements EmailValidator {
-      isValid (email: string): boolean {
-        return true
-      }
-    }
-    const emailValidatorStub = new EmailValidatorStub()
-    const sut = new EmailValidation('email', emailValidatorStub)
+    const { sut } = makeSut()
     const error = sut.validate({ email: 'any_email@mail.com' })
     expect(error).toEqual(new InvalidParamError('email'))
   })
