b54032ebf48c10ace34503b66e6052fa17bc590f
Miguel <miguelaugl@outlook.com>
2021-09-27 00:18:18 -0300

    feat: ensure SignUpController calls Validation with correct values

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,36 +1,18 @@
 import { AddAccount } from '@/domain/usecases'
-import { InvalidParamError, MissingParamError } from '@/presentation/errors'
-import { badRequest, ok, serverError } from '@/presentation/helpers'
+import { ok, serverError } from '@/presentation/helpers'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
-import { CpfValidator, EmailValidator } from '@/validation/protocols'
+import { Validation } from '@/validation/protocols'
 
 export class SignUpController implements Controller {
   constructor (
-    private readonly emailValidator: EmailValidator,
-    private readonly cpfValidator: CpfValidator,
     private readonly addAccount: AddAccount,
+    private readonly validation: Validation,
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation', 'ra', 'course', 'cpf']
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
-      }
-      const { name, email, password, passwordConfirmation, cpf, ra, course } = httpRequest.body
-      if (password !== passwordConfirmation) {
-        return badRequest(new InvalidParamError('passwordConfirmation'))
-      }
-      const isEmailValid = this.emailValidator.isValid(email)
-      if (!isEmailValid) {
-        return badRequest(new InvalidParamError('email'))
-      }
-      const isCpfValid = this.cpfValidator.isValid(cpf)
-      if (!isCpfValid) {
-        return badRequest(new InvalidParamError('cpf'))
-      }
+      this.validation.validate(httpRequest.body)
+      const { name, email, password, cpf, ra, course } = httpRequest.body
       const account = await this.addAccount.add({
         name,
         course,
