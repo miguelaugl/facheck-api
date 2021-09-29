3a882ebf2018eb4165b8d8e2eb0813d679953ecb
Miguel <miguelaugl@outlook.com>
2021-09-29 00:19:50 -0300

    test: ensure AddAccountRepository adds an account on success

---
+++ src/infra/db/mongodb/account-mongo-repository.spec.ts
@@ -0,0 +1,34 @@
+import { Collection } from 'mongodb'
+
+import { AccountMongoRepository } from './account-mongo-repository'
+import { MongoHelper } from './mongo-helper'
+
+let accountCollection: Collection
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  it('should add an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const isValid = await sut.add({
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password',
+      course: 'valid_course',
+      cpf: 'valid_cpf',
+      ra: 'valid_ra',
+    })
+    expect(isValid).toBe(true)
+  })
+})
