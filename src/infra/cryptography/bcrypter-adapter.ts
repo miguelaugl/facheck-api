005558378ed4f05c1670e02a214c1c9408278080
Miguel <miguelaugl@outlook.com>
2021-09-28 23:22:24 -0300

    feat: ensure BcrypterAdapter calls bcrypt with correct values

---
+++ src/infra/cryptography/bcrypter-adapter.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt'
+
+import { Hasher } from '@/data/protocols'
+
+export class BcrypterAdapter implements Hasher {
+  constructor (
+    private readonly salt: number,
+  ) {}
+
+  async hash (value: string): Promise<string> {
+    await bcrypt.hash(value, this.salt)
+    return null
+  }
+}
