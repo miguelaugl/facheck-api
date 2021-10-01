daa742a792a84761357223c2cbfc5114e6372901
Miguel <miguelaugl@outlook.com>
2021-09-30 22:47:19 -0300

    feat: ensure CpfValidatorAdapter returns true if validator returns true

--- src/infra/validators/cpf-validator-adapter.ts
+++ src/infra/validators/cpf-validator-adapter.ts
@@ -4,7 +4,6 @@ import { CpfValidator } from '@/validation/protocols'
 
 export class CpfValidatorAdapter implements CpfValidator {
   isValid (value: string): boolean {
-    cpf.isValid(value)
-    return null
+    return cpf.isValid(value)
   }
 }
