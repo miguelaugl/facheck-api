86fe032dcd941757b89e40acea5c4933689deba0
Miguel <miguelaugl@outlook.com>
2021-09-30 22:34:56 -0300

    refactor: remove EmailValidator dependency from EmailValidation

--- src/validation/validators/email-validation.spec.ts
+++ src/validation/validators/email-validation.spec.ts
@@ -1,36 +1,22 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { EmailValidator } from '@/validation/protocols'
 
 import { EmailValidation } from './email-validation'
 
 type SutTypes = {
   sut: EmailValidation
-  emailValidatorStub: EmailValidator
-}
-
-const makeEmailValidator = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid (email: string): boolean {
-      return true
-    }
-  }
-  return new EmailValidatorStub()
 }
 
 const makeSut = (): SutTypes => {
-  const emailValidatorStub = makeEmailValidator()
-  const sut = new EmailValidation('email', emailValidatorStub)
+  const sut = new EmailValidation('email')
   return {
     sut,
-    emailValidatorStub,
   }
 }
 
 describe('Email Validation', () => {
   it('should return an InvalidParamError if validation fails', () => {
-    const { sut, emailValidatorStub } = makeSut()
-    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-    const error = sut.validate({ email: 'any_email@mail.com' })
+    const { sut } = makeSut()
+    const error = sut.validate({ email: 'invalid_email' })
     expect(error).toEqual(new InvalidParamError('email'))
   })
 
@@ -38,12 +24,5 @@ describe('Email Validation', () => {
     const { sut } = makeSut()
     const error = sut.validate({ email: 'any_email@mail.com' })
     expect(error).toBeFalsy()
-  })
-
-  it('should call EmailValidator with correct email', async () => {
-    const { sut, emailValidatorStub } = makeSut()
-    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
-    sut.validate({ email: 'any_email@mail.com' })
-    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
--- src/validation/validators/email-validation.ts
+++ src/validation/validators/email-validation.ts
@@ -1,14 +1,14 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { EmailValidator, Validation } from '@/validation/protocols'
+import { Validation } from '@/validation/protocols'
 
 export class EmailValidation implements Validation {
   constructor (
     private readonly fieldName: string,
-    private readonly emailValidator: EmailValidator,
   ) {}
 
   validate (input: any): Error {
-    if (!this.emailValidator.isValid(input[this.fieldName])) {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    if (!emailRegex.test((input[this.fieldName]))) {
       return new InvalidParamError(this.fieldName)
     }
   }
