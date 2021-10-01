9109e7b6912849c767452d81700ec37b9f5f701a
Miguel <miguelaugl@outlook.com>
2021-09-30 22:47:35 -0300

    test: ensure CpfValidatorAdapter returns true if validator returns true

--- src/infra/validators/cpf-validator-adapter.spec.ts
+++ src/infra/validators/cpf-validator-adapter.spec.ts
@@ -2,11 +2,25 @@ import { cpf } from 'cpf-cnpj-validator'
 
 import { CpfValidatorAdapter } from './cpf-validator-adapter'
 
+jest.mock('cpf-cnpj-validator', () => ({
+  cpf: {
+    isValid (): boolean {
+      return true
+    },
+  },
+}))
+
 describe('CpfValidator Adapter', () => {
-  it('should call cpf-cnpj-validator.cpf with correct cpf', () => {
+  it('should call validator with correct cpf', () => {
     const sut = new CpfValidatorAdapter()
     const isValidSpy = jest.spyOn(cpf, 'isValid')
     sut.isValid('any_cpf')
     expect(isValidSpy).toHaveBeenCalledWith('any_cpf')
+  })
+
+  it('should returns true if validator returns true', () => {
+    const sut = new CpfValidatorAdapter()
+    const isValid = sut.isValid('valid_cpf')
+    expect(isValid).toBe(true)
   })
 })
