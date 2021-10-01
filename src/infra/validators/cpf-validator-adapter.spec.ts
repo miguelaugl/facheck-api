38def0f66f09bd818863bf18d13bcfbf10530601
Miguel <miguelaugl@outlook.com>
2021-09-30 22:49:43 -0300

    refactor: move SUT creation to a factory method

--- src/infra/validators/cpf-validator-adapter.spec.ts
+++ src/infra/validators/cpf-validator-adapter.spec.ts
@@ -10,22 +10,26 @@ jest.mock('cpf-cnpj-validator', () => ({
   },
 }))
 
+const makeSut = (): CpfValidatorAdapter => {
+  return new CpfValidatorAdapter()
+}
+
 describe('CpfValidator Adapter', () => {
   it('should call validator with correct cpf', () => {
-    const sut = new CpfValidatorAdapter()
+    const sut = makeSut()
     const isValidSpy = jest.spyOn(cpf, 'isValid')
     sut.isValid('any_cpf')
     expect(isValidSpy).toHaveBeenCalledWith('any_cpf')
   })
 
   it('should return true if validator returns true', () => {
-    const sut = new CpfValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid_cpf')
     expect(isValid).toBe(true)
   })
 
   it('should return false if validator returns false', () => {
-    const sut = new CpfValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(cpf, 'isValid').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_cpf')
     expect(isValid).toBe(false)
