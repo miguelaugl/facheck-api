c6c860d15229902d8a0c4688c29fa6c9d291fb74
Miguel <miguelaugl@outlook.com>
2021-10-15 16:33:58 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/controllers/login.spec.ts
+++ src/presentation/controllers/login.spec.ts
@@ -2,16 +2,33 @@ import { Authentication } from '@/domain/usecases'
 
 import { LoginController } from './login'
 
+const makeAuthenticationStub = (): Authentication => {
+  class AuthenticationStub implements Authentication {
+    async auth (params: Authentication.Params): Promise<Authentication.Result> {
+      return 'any_token'
+    }
+  }
+  return new AuthenticationStub()
+}
+
+type SutTypes = {
+  sut: LoginController
+  authenticationStub: Authentication
+}
+
+const makeSut = (): SutTypes => {
+  const authenticationStub = makeAuthenticationStub()
+  const sut = new LoginController(authenticationStub)
+  return {
+    sut,
+    authenticationStub,
+  }
+}
+
 describe('Login Controller', () => {
   it('should call Authentication with correct values', async () => {
-    class AuthenticationStub implements Authentication {
-      async auth (params: Authentication.Params): Promise<Authentication.Result> {
-        return 'any_token'
-      }
-    }
-    const authencationStub = new AuthenticationStub()
-    const sut = new LoginController(authencationStub)
-    const authSpy = jest.spyOn(authencationStub, 'auth')
+    const { sut, authenticationStub } = makeSut()
+    const authSpy = jest.spyOn(authenticationStub, 'auth')
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
