62b0803a751a0e157d2f26ef45069d3f6c6005c8
Miguel <miguelaugl@outlook.com>
2021-10-16 14:59:24 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct accessToken

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,12 +1,37 @@
+import { mockAccountModel } from '@/domain/tests'
+import { LoadAccountByToken } from '@/domain/usecases'
 import { AccessDeniedError } from '@/presentation/errors'
 import { forbidden } from '@/presentation/helpers'
 
 import { AuthMiddleware } from './auth-middleware'
 
+class LoadAccountByTokenSpy implements LoadAccountByToken {
+  accessToken: string
+  result = mockAccountModel()
+
+  async loadAccountByToken (accessToken: string): Promise<LoadAccountByToken.Result> {
+    this.accessToken = accessToken
+    return this.result
+  }
+}
+
 describe('Auth Middleware', () => {
   it('should return 403 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware()
+    const loadAccountByTokenSpy = new LoadAccountByTokenSpy()
+    const sut = new AuthMiddleware(loadAccountByTokenSpy)
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  it('should call LoadAccountByToken with correct accessToken', async () => {
+    const loadAccountByTokenSpy = new LoadAccountByTokenSpy()
+    const sut = new AuthMiddleware(loadAccountByTokenSpy)
+    const accessToken = 'any_token'
+    await sut.handle({
+      headers: {
+        'x-access-token': accessToken,
+      },
+    })
+    expect(loadAccountByTokenSpy.accessToken).toEqual(accessToken)
   })
 })
