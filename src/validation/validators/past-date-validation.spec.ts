cae5c16833f3e8424fd8533e2e5c908af88b1551
Miguel <miguelaugl@outlook.com>
2021-10-16 23:47:09 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/past-date-validation.spec.ts
+++ src/validation/validators/past-date-validation.spec.ts
@@ -2,10 +2,14 @@ import { InvalidParamError } from '@/presentation/errors'
 
 import { PastDateValidation } from './past-date-validation'
 
+const makeSut = (fieldName = 'date'): PastDateValidation => {
+  return new PastDateValidation(fieldName)
+}
+
 describe('PastDate Validation', () => {
   it('should return an InvalidParamError if validation fails', () => {
     const fieldName = 'date'
-    const sut = new PastDateValidation(fieldName)
+    const sut = makeSut(fieldName)
     const error = sut.validate({ [fieldName]: 'any_date' })
     expect(error).toEqual(new InvalidParamError(fieldName))
   })
