51d3850245e92e892181ef6f3d7363c70a6786d2
Miguel <miguelaugl@outlook.com>
2021-10-16 23:53:44 -0300

    feat: ensure PastDateValidation not returns if validation succeeds

--- src/validation/validators/past-date-validation.ts
+++ src/validation/validators/past-date-validation.ts
@@ -2,9 +2,12 @@ import { InvalidParamError } from '@/presentation/errors'
 import { Validation } from '@/validation/protocols'
 
 export class PastDateValidation implements Validation {
-  constructor (private readonly fieldName: string) {}
+  constructor (private readonly fieldName: string, private readonly today = new Date()) {}
 
   validate (input: any): Error {
-    return new InvalidParamError(this.fieldName)
+    const isPastDate = this.today.getTime() > input[this.fieldName].getTime()
+    if (isPastDate) {
+      return new InvalidParamError(this.fieldName)
+    }
   }
 }
