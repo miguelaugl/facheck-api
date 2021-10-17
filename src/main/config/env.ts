c701fa87b330f98eed5120062e8ab46d498c3fea
Miguel <miguelaugl@outlook.com>
2021-10-17 01:15:46 -0300

    feat: rename mongoUrl

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/facheck-api',
   port: process.env.PORT || 5040,
   jwtSecretKey: process.env.JWT_SECRET_KEY || '311cd9fb97b28f0bf092f1ba7ccda113',
 }
