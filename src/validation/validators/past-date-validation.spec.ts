fec7d166e3f2fe8126fc5febaa7614140157a752
Miguel <miguelaugl@outlook.com>
2021-10-16 23:54:13 -0300

    test: ensure PastDateValidation not returns if validation succeeds

--- src/validation/validators/past-date-validation.spec.ts
+++ src/validation/validators/past-date-validation.spec.ts
@@ -2,6 +2,8 @@ import { InvalidParamError } from '@/presentation/errors'
 
 import { PastDateValidation } from './past-date-validation'
 
+const ONE_DAY_IN_MILISECONDS = 1000 * 60 * 60 * 24
+
 const makeSut = (fieldName = 'date'): PastDateValidation => {
   return new PastDateValidation(fieldName)
 }
@@ -9,8 +11,15 @@ const makeSut = (fieldName = 'date'): PastDateValidation => {
 describe('PastDate Validation', () => {
   it('should return an InvalidParamError if validation fails', () => {
     const fieldName = 'date'
+    const yesterday = new Date((new Date()).valueOf() - ONE_DAY_IN_MILISECONDS)
     const sut = makeSut(fieldName)
-    const error = sut.validate({ [fieldName]: 'any_date' })
+    const error = sut.validate({ [fieldName]: yesterday })
     expect(error).toEqual(new InvalidParamError(fieldName))
+  })
+
+  it('should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ date: new Date() })
+    expect(error).toBeFalsy()
   })
 })
