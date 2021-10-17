e01ebe1472eeea933e8caff4299f2890c85f52a6
Miguel <miguelaugl@outlook.com>
2021-10-17 19:58:51 -0300

    feat: ensure MonitoringMongoRepository loads a monitoring by id on success

--- src/domain/tests/mock-monitoring.ts
+++ src/domain/tests/mock-monitoring.ts
@@ -2,7 +2,7 @@ import { MonitoringModel } from '@/domain/models'
 import { AddMonitoring } from '@/domain/usecases'
 
 export const mockMonitoringModel = (): MonitoringModel => ({
-  id: 'any_id',
+  id: '616c65640e31a2f85ca4eb01',
   monitorId: 'any_monitor_id',
   subject: 'any_subject',
   initDate: new Date(),
--- src/infra/db/mongodb/monitoring/monitoring-mongo-repository.ts
+++ src/infra/db/mongodb/monitoring/monitoring-mongo-repository.ts
@@ -1,7 +1,9 @@
-import { AddMonitoringRepository, LoadMonitoringsRepository } from '@/data/protocols'
+import { ObjectId } from 'bson'
+
+import { AddMonitoringRepository, LoadMonitoringByIdRepository, LoadMonitoringsRepository } from '@/data/protocols'
 import { MongoHelper } from '@/infra/db/mongodb'
 
-export class MonitoringMongoRepository implements AddMonitoringRepository, LoadMonitoringsRepository {
+export class MonitoringMongoRepository implements AddMonitoringRepository, LoadMonitoringsRepository, LoadMonitoringByIdRepository {
   async add (params: AddMonitoringRepository.Params): Promise<void> {
     const monitoringsCollection = MongoHelper.getCollection('monitorings')
     await monitoringsCollection.insertOne(params)
@@ -11,5 +13,13 @@ export class MonitoringMongoRepository implements AddMonitoringRepository, LoadM
     const monitoringsCollection = MongoHelper.getCollection('monitorings')
     const monitorings = await monitoringsCollection.find().toArray()
     return monitorings && MongoHelper.mapCollection(monitorings)
+  }
+
+  async loadById (monitoringId: string): Promise<LoadMonitoringByIdRepository.Result> {
+    const monitoringsCollection = MongoHelper.getCollection('monitorings')
+    const monitoring = await monitoringsCollection.findOne({
+      _id: new ObjectId(monitoringId),
+    })
+    return monitoring && MongoHelper.map(monitoring)
   }
 }
