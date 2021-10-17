60d8b607b91f995613c6b5b931934a2f80465329
Miguel <miguelaugl@outlook.com>
2021-10-17 20:14:24 -0300

    feat: ensure LoadMonitoringByIdController returns 403 if LoadMonitoringById returns null

--- src/presentation/controllers/monitoring/load-monitoring-by-id.ts
+++ src/presentation/controllers/monitoring/load-monitoring-by-id.ts
@@ -1,5 +1,6 @@
 import { LoadMonitoringById } from '@/domain/usecases'
-import { ok, serverError } from '@/presentation/helpers'
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden, ok, serverError } from '@/presentation/helpers'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class LoadMonitoringByIdController implements Controller {
@@ -10,6 +11,9 @@ export class LoadMonitoringByIdController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const monitoring = await this.loadMonitoringById.load(httpRequest.params?.monitoringId)
+      if (!monitoring) {
+        return forbidden(new InvalidParamError('monitoringId'))
+      }
       return ok(monitoring)
     } catch (error) {
       return serverError(error)
