13744ec15238e176007158d1453fd1c5c98e43a2
Miguel <miguelaugl@outlook.com>
2021-10-17 19:02:31 -0300

    feat: ensure LoadMonitorings returns 500 if LoadMonitorings throws

--- src/presentation/controllers/monitoring/load-monitorings.ts
+++ src/presentation/controllers/monitoring/load-monitorings.ts
@@ -1,11 +1,16 @@
 import { LoadMonitorings } from '@/domain/usecases'
+import { serverError } from '@/presentation/helpers'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class LoadMonitoringsController implements Controller {
   constructor (private readonly loadMonitorings: LoadMonitorings) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadMonitorings.load()
-    return null
+    try {
+      await this.loadMonitorings.load()
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
