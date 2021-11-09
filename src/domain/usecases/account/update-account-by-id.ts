8facdb216ded528a36a190cdab1935bff5fd3bac
Miguel <miguelaugl@outlook.com>
2021-11-09 19:30:19 -0300

    feat: add UpdateAccountById use case

---
+++ src/domain/usecases/account/update-account-by-id.ts
@@ -0,0 +1,16 @@
+import { AccountModel } from '@/domain/models'
+
+export interface UpdateAccountById {
+  update: (accountId: string, accountData: UpdateAccountById.Params) => Promise<UpdateAccountById.Result>
+}
+
+export namespace UpdateAccountById {
+  export type Params = {
+    name: string
+    email: string
+    ra: string
+    course: string
+    cpf: string
+  }
+  export type Result = AccountModel
+}
